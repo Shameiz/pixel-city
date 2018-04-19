74865fb3ead567f649ba8ed66633a6d3a6fa6b40
Shameiz Rangwala <srangwal@edu.uwaterloo.ca>
2018-04-19 12:30:34 -0400

    displayed pictures from Flickr in collection view

--- pixel-city/MapVC.swift
+++ pixel-city/MapVC.swift
@@ -42,9 +42,10 @@ class MapVC: UIViewController,UIGestureRecognizerDelegate {
         collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "photoCell")
         collectionView?.delegate=self;
         collectionView?.dataSource=self;
-        collectionView?.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        collectionView?.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 1)
         
         pullUpView.addSubview(collectionView!)
+        registerForPreviewing(with: self, sourceView: collectionView!)
         // Do any additional setup after loading the view, typically from a nib.
     }
     
@@ -70,6 +71,8 @@ class MapVC: UIViewController,UIGestureRecognizerDelegate {
         
     @objc func animateViewDown(){
         cancelAllSessions()
+        imageUrlArray = []
+        imageArray = []
         pullUpViewHeightConstraint.constant = 0;
         UIView.animate(withDuration: 0.3) {
             self.view.layoutIfNeeded()
@@ -142,6 +145,9 @@ extension MapVC: MKMapViewDelegate{
         removeSpinner()
         removeProgressLabel()
         cancelAllSessions()
+        imageArray = []
+        imageUrlArray = []
+        collectionView?.reloadData()
         
         animateViewUp()
         addSwipe()
@@ -163,6 +169,7 @@ extension MapVC: MKMapViewDelegate{
                     if (success){
                         self.removeSpinner()
                         self.removeProgressLabel()
+                        self.collectionView?.reloadData()
                     }
                 })
             }
@@ -176,7 +183,6 @@ extension MapVC: MKMapViewDelegate{
     }
     
     func retrieveUrls(forAnnotation annotation:DroppablePin, handler: @escaping CompletionHandler){
-        imageUrlArray = []
         Alamofire.request(flickUrl(forApiKey: apiKey, withAnnotation: annotation, addNumberofPhotos: 40)).responseJSON { (response) in
             guard let json = response.result.value as? Dictionary<String,AnyObject> else{return}
             let phtosDict = json["photos"] as! Dictionary<String,AnyObject>
@@ -191,7 +197,6 @@ extension MapVC: MKMapViewDelegate{
     }
     
     func retrieveImages(handler : @escaping CompletionHandler){
-        imageArray = []
         for url in imageUrlArray{
             Alamofire.request(url).responseImage(completionHandler: { (response) in
                 guard let image = response.result.value else {return}
@@ -233,12 +238,37 @@ extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource{
         return 1;
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4;
+        return imageArray.count;
     }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell
-        return cell!
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "photoCell", for: indexPath) as? PhotoCell else{return UICollectionViewCell()}
+        let imageFromIndex = imageArray[indexPath.row]
+        let imageView = UIImageView(image: imageFromIndex)
+        cell.addSubview(imageView)
+        return cell
     }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        guard let popvc = storyboard?.instantiateViewController(withIdentifier: "PopVC") as? PopVC else{return}
+        popvc.initData(forImage: imageArray[indexPath.row])
+        present(popvc, animated: true, completion: nil)
+    }
+}
+
+extension MapVC :UIViewControllerPreviewingDelegate{
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, viewControllerForLocation location: CGPoint) -> UIViewController? {
+        guard let indexPath = collectionView?.indexPathForItem(at: location), let cell = collectionView?.cellForItem(at: indexPath) else{return nil}
+        guard let popvc = storyboard?.instantiateViewController(withIdentifier: "PopVC") as? PopVC else {return nil}
+        popvc.initData(forImage: imageArray[indexPath.row])
+        previewingContext.sourceRect = cell.contentView.frame
+        return popvc
+    }
+    
+    func previewingContext(_ previewingContext: UIViewControllerPreviewing, commit viewControllerToCommit: UIViewController) {
+        show(viewControllerToCommit, sender: self)
+    }
+    
+    
 }
 
 
